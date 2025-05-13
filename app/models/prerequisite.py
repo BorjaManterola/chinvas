2c7d96c78ef20f0d3543edc6f3198fae4e86c8b8
vicenterey <virey@miuandes.cl>
2025-05-12 23:13:14 -0400

    arreglando delete course

--- app/models/prerequisite.py
+++ app/models/prerequisite.py
@@ -6,5 +6,5 @@ class Prerequisite(db.Model):
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id', ondelete='CASCADE'), nullable=False)
     prerequisite_id = db.Column(db.Integer, db.ForeignKey('courses.id', ondelete='CASCADE'), nullable=False)
 
-    course = db.relationship('Course', foreign_keys=[course_id], cascade="all, delete", backref='prerequisites')
-    prerequisite = db.relationship('Course', foreign_keys=[prerequisite_id], backref='prerequisite_of')
+    course = db.relationship('Course', foreign_keys=[course_id], backref=db.backref('prerequisites', cascade="all, delete-orphan", lazy=True))
+    prerequisite = db.relationship('Course', foreign_keys=[prerequisite_id], backref=db.backref('prerequisite_of', cascade="all, delete-orphan", lazy=True))
