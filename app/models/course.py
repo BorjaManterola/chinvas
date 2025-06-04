73aaf2d31bae13eb1f2f76a946bb54c5d75978d9
Borja Manterola <bjmanterola@miuandes.cl>
2025-06-04 14:27:41 -0400

    minor refactor

--- app/models/course.py
+++ app/models/course.py
@@ -1,11 +1,23 @@
 from app import db
 
+
 class Course(db.Model):
-    __tablename__ = 'courses'
+    __tablename__ = "courses"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     code = db.Column(db.String(50), nullable=False)
     credits = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+    created_at = db.Column(
+        db.DateTime, server_default=db.func.current_timestamp()
+    )
 
-    periods = db.relationship("Period", backref="course", cascade="all, delete", lazy=True)
+    periods = db.relationship(
+        "Period", backref="course", cascade="all, delete", lazy=True
+    )
+
+    @staticmethod
+    def get_unassigned_courses(course_id, assigned_ids):
+        unassigned_courses = Course.query.filter(
+            Course.id != course_id, ~Course.id.in_(assigned_ids)
+        ).all()
+        return unassigned_courses
